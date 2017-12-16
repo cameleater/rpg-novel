cc6aa37e7f64ce7300f499d550de7869de877346
Jordan <jordann.jn@gmail.com>
2017-12-16 16:18:15 +0700

    first commit

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCKOwk7-cYFNtGwHRij2ergml0lhc-eOzM",
+    authDomain: "bsbapp-d6a70.firebaseapp.com",
+    databaseURL: "https://bsbapp-d6a70.firebaseio.com",
+    projectId: "bsbapp-d6a70",
+    storageBucket: "bsbapp-d6a70.appspot.com",
+    messagingSenderId: "185058715076"
+  };
